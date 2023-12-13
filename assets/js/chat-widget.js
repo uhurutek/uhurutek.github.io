a38b8567ce80fae41ea86317e497417befd3bbc3
shant00 <sshanto.cse@gmail.com>
2023-12-13 20:25:59 +0600

    finished up initilas type handle

--- assets/js/chat-widget.js
+++ assets/js/chat-widget.js
@@ -1,41 +1,42 @@
 (function () {
+
   // Inject the CSS
   const style = document.createElement('style');
   style.innerHTML = `
   .w-16 {
-    width: 4rem; /* Sets width to 16 pixels */
+    width: 4rem; 
   }
   
   .h-16 {
-    height: 4rem; /* Sets height to 16 pixels */
+    height: 4rem; 
   }
   
   .bg-gray-800 {
-    background-color: #9f4923; /* Sets background color to a shade of gray */
+    background-color: #9f4923; 
   }
   
   .rounded-full {
-    border-radius: 9999px; /* Creates a circular shape using a large border-radius */
+    border-radius: 9999px; 
   }
   
   .flex {
-    display: flex; /* Uses flexbox for layout */
+    display: flex; 
   }
   
   .items-center {
-    align-items: center; /* Centers items along the cross-axis (vertically in this case) */
+    align-items: center; 
   }
   
   .justify-center {
-    justify-content: center; /* Centers items along the main axis (horizontally in this case) */
+    justify-content: center;
   }
   
   .cursor-pointer {
-    cursor: pointer; /* Changes cursor to a pointer on hover */
+    cursor: pointer; 
   }
   
   .text-3xl {
-    font-size: 1.875rem; /* Sets font size to 3xl */
+    font-size: 1.875rem;
   }  
   #chat-widget-container {
     position: fixed;
@@ -63,27 +64,27 @@
     }
   }
   .w-10 {
-    width: 2.5rem; /* Sets width to 10 pixels */
+    width: 2.5rem; 
   }
   
   .h-10 {
-    height: 2.5rem; /* Sets height to 10 pixels */
+    height: 2.5rem; 
   }
   
   .text-white {
-    color: #ffffff; /* Sets text color to white */
+    color: #ffffff; 
   }
   
   .absolute {
-    position: absolute; /* Positions the element absolutely */
+    position: absolute; 
   }
   
   .bottom-20 {
-    bottom: 5rem; /* Positions the element 20 pixels from the bottom */
+    bottom: 5rem; 
   }
   
   .right-0 {
-    right: 0; /* Positions the element at the right side of its container */
+    right: 0; 
   }
   
   .w-96 {
@@ -127,195 +128,261 @@
     background-color: #ffffff; 
   }
   .rounded-md {
-    border-radius: 0.375rem; /* Applies a medium rounded border */
+    border-radius: 0.375rem; 
   }
   .shadow-md {
-    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); /* Adds a shadow effect */
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); 
   }
   .flex {
-    display: flex; /* Uses flexbox for layout */
+    display: flex; 
   }
   .flex-col {
-    flex-direction: column; /* Sets flex direction to column */
+    flex-direction: column; 
   }
   .transition-all {
-    transition-property: all; /* Specifies transitioning for all properties */
+    transition-property: all; 
   }
   .text-sm {
-    font-size: 0.875rem; /* Sets font size to small */
+    font-size: 0.875rem; 
   }
   .flex {
-    display: flex; /* Uses flexbox for layout */
+    display: flex; 
   }
   .justify-between {
-    justify-content: space-between; /* Distributes items along the main axis, with space between */
+    justify-content: space-between; 
   }
   .items-center {
-    align-items: center; /* Centers items along the cross-axis (vertically in this case) */
+    align-items: center; 
   }
   .p-4 {
-    padding: 1rem; /* Applies padding of 4 units */
+    padding: 1rem; 
   }
   .bg-gray-800 {
-    background-color: #9f4923; /* Sets background color to a shade of gray */
+    background-color: #9f4923; 
   }
   .text-white {
-    color: #ffffff; /* Sets text color to white */
+    color: #ffffff; 
   }
   .rounded-t-md {
-    border-top-left-radius: 0.375rem; /* Applies medium rounded border to the top left corner */
-    border-top-right-radius: 0.375rem; /* Applies medium rounded border to the top right corner */
+    border-top-left-radius: 0.375rem; 
+    border-top-right-radius: 0.375rem;
   }
   .bg-transparent {
-    background-color: transparent; /* Sets background color to transparent */
+    background-color: transparent; 
   }
   .border-none {
-    border: none; /* Removes the border */
+    border: none; 
   }
   .text-white {
-    color: #ffffff; /* Sets text color to white */
+    color: #ffffff;
   }
   .cursor-pointer {
-    cursor: pointer; /* Changes cursor to a pointer on hover */
+    cursor: pointer;
   }
   .flex-1 {
-    flex: 1; /* Allows the element to grow and fill available space */
+    flex: 1; 
   }
   .border {
-    border-width: 1px; /* Applies a border with the default color */
+    border-width: 1px; 
   }
   .border-gray-300 {
-    border-color: #9f4923; /* Sets border color to a shade of gray */
+    border-color: #9f4923; 
   }
   .rounded-md {
-    border-radius: 0.375rem; /* Applies medium rounded corners */
+    border-radius: 0.375rem; 
   }
   .px-4 {
-    padding-left: 1rem; /* Applies horizontal padding of 4 units */
+    padding-left: 1rem; 
     padding-right: 1rem;
   }
   .py-2 {
-    padding-top: 0.5rem; /* Applies vertical padding of 2 units */
+    padding-top: 0.5rem; 
     padding-bottom: 0.5rem;
   }
   .outline-none {
-    outline: none; /* Removes the outline when focused */
+    outline: none; 
   }
   .w-3/4 {
-    width: 75%; /* Sets width to 75% */
+    width: 75%; 
   }
   .overflow-y-auto {
-    overflow-y: auto; /* Enables vertical scrollbar if needed */
+    overflow-y: auto; 
   }
   .border-t {
-    border-top-width: 1px; /* Applies a top border */
-    border-color: #9f4923; /* Sets top border color */
+    border-top-width: 1px; 
+    border-color: #9f4923; 
   }
   .border-gray-200 {
-    border-color: #9f4923; /* Sets border color to a lighter shade of gray */
+    border-color: #9f4923; 
   }
   .hidden {
     visibility: hidden;
   }
   .space-x-4 {
-    margin-right: 1rem; /* Applies horizontal margin between child elements */
+    margin-right: 1rem; 
   }
   
   .items-center {
-    align-items: center; /* Centers items along the cross-axis (vertically in this case) */
+    align-items: center; 
   }
   .text-indigo-600 {
-    color: #7f9cf5; /* Sets text color to an indigo shade */
+    color: #7f9cf5; 
   }
   
   .flex {
-    display: flex; /* Uses flexbox for layout */
+    display: flex; 
   }
   
   .text-center {
-    text-align: center; /* Centers text horizontally */
+    text-align: center; 
   }
   
   .text-xs {
-    font-size: 0.75rem; /* Sets font size to extra small */
+    font-size: 0.75rem; 
   }
   
   .pt-4 {
-    padding-top: 1rem; /* Applies padding-top of 4 units */
+    padding-top: 1rem; 
   }
   
   .bg-gray-800 {
-    background-color: #9f4923; /* Sets background color to a shade of gray */
+    background-color: #9f4923; 
   }
   
   .text-white {
-    color: #ffffff; /* Sets text color to white */
+    color: #ffffff; 
   }
   
   .rounded-md {
-    border-radius: 0.375rem; /* Applies medium rounded corners */
+    border-radius: 0.375rem; 
   }
   
   .px-4 {
-    padding-left: 1rem; /* Applies horizontal padding of 4 units */
+    padding-left: 1rem; 
     padding-right: 1rem;
   }
   
   .py-2 {
-    padding-top: 0.5rem; /* Applies vertical padding of 2 units */
+    padding-top: 0.5rem; 
     padding-bottom: 0.5rem;
   }
   
   .cursor-pointer {
-    cursor: pointer; /* Changes cursor to a pointer on hover */
+    cursor: pointer; 
   }
   
   .bg-gray-200 {
-    background-color: #9f4923; /* Sets background color to a light gray */
+    background-color: #9f4923; 
   }
   
   .text-black {
-    color: #000000; /* Sets text color to black */
+    color: #000000; 
   }
   
   .rounded-lg {
-    border-radius: 0.5rem; /* Applies large rounded corners */
+    border-radius: 0.5rem; 
   }
   
   .py-2 {
-    padding-top: 0.5rem; /* Applies vertical padding of 2 units */
+    padding-top: 0.5rem; 
     padding-bottom: 0.5rem;
   }
   
   .px-4 {
-    padding-left: 1rem; /* Applies horizontal padding of 4 units */
+    padding-left: 1rem; 
     padding-right: 1rem;
   }
   
   .max-w-[70%] {
-    max-width: 70%; /* Sets maximum width to 70% */
+    max-width: 70%; 
   }
   
   .bg-gray-800 {
-    background-color: #9f4923; /* Sets background color to a dark gray */
+    background-color: #9f4923; 
   }
   
   .text-white {
-    color: #ffffff; /* Sets text color to white */
+    color: #ffffff; 
   }
   
   .flex {
-    display: flex; /* Uses flexbox for layout */
+    display: flex; 
   }
   
   .justify-end {
-    justify-content: flex-end; /* Aligns items to the end of the main axis */
+    justify-content: flex-end; 
   }
   
   .mb-3 {
-    margin-bottom: 0.75rem; /* Applies margin-bottom of 3 units */
+    margin-bottom: 0.75rem; 
   }
-  
+  .bot-card {
+    width: 300px;
+    padding: 20px;
+    border: 1px solid #9f4923;
+    border-radius: 8px;
+    text-align: center;
+  }
+
+  .bot-card img {
+      width: 100%;
+      height: auto;
+      margin-bottom: 15px;
+  }
+
+  .bot-actions a {
+      display: block;
+      margin-bottom: 10px;
+      text-decoration: none;
+      font-weight: bold;
+  }
+  .bot-carousel {
+    position: relative;
+    max-width: 600px;
+    margin: auto;
+    overflow: hidden;
+  }
+
+  .bot-carousel-container {
+    display: flex;
+    transition: transform 0.5s ease;
+  }
+
+  .bot-slide {
+    min-width: 100%;
+    display: none;
+  }
+
+  .bot-slide img {
+    width: 100%;
+    height: auto;
+  }
+
+  .bot-slide:first-child {
+    display: block;
+  }
+
+  .bot-prev,
+  .bot-next {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    background: rgba(0, 0, 0, 0.5);
+    color: white;
+    border: none;
+    cursor: pointer;
+    padding: 10px;
+    z-index: 1;
+  }
+
+  .bot-prev {
+    left: 0;
+  }
+
+  .bot-next {
+    right: 0;
+  }
+
   
   `;
   const faScript = document.createElement('script');
@@ -356,12 +423,12 @@
         
         </div>
         <div class="flex text-center text-xs pt-4">
-          <span class="flex-1">Copyright by <strong><a href="https://uhurutek.com" target="_blank" >UhuruTek</a></strong></span>
+          <span class="flex-1">Powered by <strong><a href="https://uhurutek.com" target="_blank" ><img src="https://uhurutek.com/assets/img/uhurutek_logo.svg" width="90" alt="Uhurutek Logo">
+          </a></strong></span>
         </div>
       </div>
     </div>
   `;
-
 
   // Add event listeners
   const chatInput = document.getElementById('chat-input');
@@ -396,22 +463,24 @@
   });
 
   function togglePopup() {
-    console.log("object");
     chatPopup.classList.toggle('hidden');
     if (!chatPopup.classList.contains('hidden')) {
       document.getElementById('chat-input').focus();
     }
   }
-  document.addEventListener('click', function (event) {
-    const chatPopup = document.getElementById('chat-popup');
-    const chatBubble = document.getElementById('chat-bubble');
 
-    if (!chatPopup.contains(event.target) && !chatBubble.contains(event.target)) {
-      if (!chatPopup.classList.contains('hidden')) {
-        chatPopup.classList.add('hidden');
-      }
-    }
-  });
+  /* without modal click close modal */
+
+  // document.addEventListener('click', function (event) {
+  //   const chatPopup = document.getElementById('chat-popup');
+  //   const chatBubble = document.getElementById('chat-bubble');
+
+  //   if (!chatPopup.contains(event.target) && !chatBubble.contains(event.target)) {
+  //     if (!chatPopup.classList.contains('hidden')) {
+  //       chatPopup.classList.add('hidden');
+  //     }
+  //   }
+  // });
 
   function onUserRequest(message) {
     // Display user message
@@ -426,50 +495,176 @@
     chatMessages.scrollTop = chatMessages.scrollHeight;
 
     chatInput.value = '';
-    const endpoint = 'https://bp.uhurutek.com/api/v1/bots/uhurubot/converse/mahabubu470@gmail.com';
-    const data = {
-      type: 'text',
-      text: message
-    };
+    fetchBotResponse(message)
+  }
 
-    fetch(endpoint, {
+  function fetchBotResponse(message) {
+    fetch('https://bp.uhurutek.com/api/v1/bots/uhurubot/converse/mahabubu470@gmail.com', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify({
+        type: 'text',
+        text: message
+      })
     })
       .then(response => response.json())
       .then(data => {
-        const responses = data.responses;
-        responses.forEach(response => {
-          if (response.type === 'text') {
-            reply(response.text);
-          } else {
-            reply("Bang hoi chua hoc cach tra loi cau hoi nay.");
-          }
-        });
+        decisionFileTypes(data.responses)
       })
       .catch(error => {
         console.error('Error:', error);
       });
+  }
+  //! TODO File type need to be define high level
+  function decisionFileTypes(responses) {
+    responses.forEach(response => {
+      if (response.type === 'text') {
+        replyText(response.text);
+      } else if (response.type === 'file') {
+        replyFile(response);
+      } else if (response.type === 'video') {
+        replyVideo(response);
+      } else if (response.type === 'card') {
+        replyCard(response);
 
-    // Reply to the user
-    // setTimeout(function () {
-    //   reply('Hello! This is a sample reply.');
-    // }, 1000);
+      } else if (response.type === "carousel") {
+        replayCarousel(response)
+      } else if (response.type === "audio") {
+        replyAudio(response)
+      }
+      else {
+        replyText('Sorry, I did not understand your request. Please try again.');
+      }
+    });
   }
 
-  function reply(message) {
+  function replyText(message) {
+    message = message.replace(/(?:\r\n|\r|\n)/g, '<br>');
     const chatMessages = document.getElementById('chat-messages');
     const replyElement = document.createElement('div');
     replyElement.className = 'flex mb-3';
     replyElement.innerHTML = `
       <div style="border: 1px solid #9f4923;" class=" text-black rounded-lg py-2 px-4 max-w-[70%]">
-        ${message}
+       <p> ${message}</p>
       </div>
     `;
     chatMessages.appendChild(replyElement);
     chatMessages.scrollTop = chatMessages.scrollHeight;
   }
+
+  function replyFile(file) {
+    const chatMessages = document.getElementById('chat-messages');
+    const replyElement = document.createElement('div');
+    replyElement.className = 'flex mb-3';
+    replyElement.innerHTML = `
+      <div style="border: 1px solid #9f4923;" class=" text-black rounded-lg py-2 px-4 max-w-[70%]">
+        <a href="${file.file}" target="_blank">${file.title}</a>
+      </div>
+    `;
+    chatMessages.appendChild(replyElement);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  }
+
+  function replyVideo(video) {
+    const chatMessages = document.getElementById('chat-messages');
+    const replyElement = document.createElement('div');
+    replyElement.className = 'flex mb-3';
+    replyElement.innerHTML = `
+      <div style="border: 1px solid #9f4923;" class=" text-black rounded-lg py-2 px-4 max-w-[70%]">
+        <video width="320" height="240" controls>
+          <source src="${video.video}" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    `;
+    chatMessages.appendChild(replyElement);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  }
+  function replyCard(cards) {
+    const chatMessages = document.getElementById('chat-messages');
+    const replyElement = document.createElement('div');
+    replyElement.className = 'flex mb-3';
+
+    const actions = cards.actions.map(response => `<a href="${response.url}" target="_blank">${response.title}</a>`).join('');
+
+    replyElement.innerHTML = `
+        <div class="bot-card">
+            <h6>${cards.title}</h6>
+            <p>${cards.subtitle}</p>
+            <img src="${cards.image}" alt="Image">
+            <div class="bot-actions">
+                ${actions}
+            </div>
+        </div>
+    `;
+
+    chatMessages.appendChild(replyElement);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  }
+  function replyAudio(audio) {
+    const chatMessages = document.getElementById('chat-messages');
+    const replyElement = document.createElement('div');
+    replyElement.className = 'flex mb-3';
+    replyElement.innerHTML = `
+      <div style="border: 1px solid #9f4923;" class=" text-black rounded-lg py-2 px-4 max-w-[70%]">
+        <audio controls>
+          <source src="${audio.audio}" type="audio/mpeg">
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+    `;
+    chatMessages.appendChild(replyElement);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  }
+  function replayCarousel(carousels) {
+    const chatMessages = document.getElementById('chat-messages');
+    const replyElement = document.createElement('div');
+    replyElement.className = 'flex mb-3';
+    // Create a variable to keep track of the current card index
+    let currentCardIndex = 0;
+    function showCard(index) {
+      const cards = carousels.items[index];
+      const actions = cards.actions.map(response => `<a href="${response.url}" target="_blank">${response.title}</a>`).join('');
+
+      replyElement.innerHTML = `
+            <div class="bot-carousel">
+                <div class="bot-carousel-container">
+                    <div class="bot-card">
+                        <h6>${cards.title}</h6>
+                        <p>${cards.subtitle}</p>
+                        <img src="${cards.image}" alt="Image">
+                        <div class="bot-actions">
+                            ${actions}
+                        </div>
+                    </div>
+                </div>
+                <button class="bot-prev">&#10094;</button>
+                <button class="bot-next">&#10095;</button>
+            </div>
+        `;
+    }
+    function showNextCard() {
+      currentCardIndex = (currentCardIndex + 1) % carousels.items.length;
+      showCard(currentCardIndex);
+    }
+    function showPreviousCard() {
+      currentCardIndex = (currentCardIndex - 1 + carousels.items.length) % carousels.items.length;
+      showCard(currentCardIndex);
+    }
+    replyElement.addEventListener('click', (event) => {
+      if (event.target.classList.contains('bot-next')) {
+        showNextCard();
+      } else if (event.target.classList.contains('bot-prev')) {
+        showPreviousCard();
+      }
+    });
+    // Show the initial card
+    showCard(currentCardIndex);
+    chatMessages.appendChild(replyElement);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  }
+
+
 })();
